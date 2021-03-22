30fbcf71ac76bf567acd7397292ff51e4430c3cf
kvazargius <novitskiybronislav@yandex.ru>
2021-03-22 17:05:37 +0300

    Добавлена функция минус

--- First/nums.py
+++ First/nums.py
@@ -2,12 +2,14 @@ def plus(a, b):
     return a + b
 
 
-text = 'new text'
-
 def minus(a, b):
     return a - b
 
 
+def nul(a, b):
+    return a * b
+
 if __name__ == '__main__':
     print(plus(2, 2))
     print(minus(4, 2))
+    print(nul(3, 3))
